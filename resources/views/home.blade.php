d4c408b4fd5bf2c7701d9fe4dfd76ef072882aec
Cianuro-83 <buzzeosimone@yahoo.com>
2023-04-04 16:40:40 +0200

    CREAZIONE ROTTA HOME

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME: LARAVEL PRIMI PASSI</title>
+
+
+</head>
+
+<body>
+    <h1>CIAO MONDO</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam necessitatibus dolorum ex, quis cupiditate optio
+        et recusandae nam? Consequuntur omnis animi totam illo rerum. Iure perferendis numquam dolores rem alias
+        nesciunt unde delectus cumque? Voluptas vel, eligendi eaque magnam sequi nesciunt quia illo magni minus
+        veritatis dolores temporibus, et odit.</p>
+    <button><a href="contatti">CONTATTI</a></button>
+</body>
+
+</html>
